de9d5c9f81cd030c4f506cf1e0b0c80fb49b397e
v2m <v2.cloud@gmail.com>
2017-02-06 22:43:49 +0800

    add carthage support

---
+++ FTFrameWork/FTFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  FTFrameWork.h
+//  FTFrameWork
+//
+//  Created by vee on 2017/2/6.
+//  Copyright © 2017年 vee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <VForceTouchGestureRecognizer.h>
+
+//! Project version number for FTFrameWork.
+FOUNDATION_EXPORT double FTFrameWorkVersionNumber;
+
+//! Project version string for FTFrameWork.
+FOUNDATION_EXPORT const unsigned char FTFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTFrameWork/PublicHeader.h>
